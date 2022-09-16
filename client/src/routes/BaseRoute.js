ceebd6022f017cd8c498abc3085caecaa7da7257
Precious2912 <preciousovat@gmail.com>
2022-09-16 18:28:35 +0100

    protect-frontend-routes

--- client/src/routes/BaseRoute.js
+++ client/src/routes/BaseRoute.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import {LandingPage} from '../pages/LandingPage'
 import { ProtectedRoute } from './ProtectedRoute';
+import { LandingPage } from "../pages/LandingPage";
+import {RegisterPage} from '../pages/RegisterPage';
+import {LoginPage} from '../pages/LoginPage';
+import {DashboardPage} from '../pages/DashboardPage'
 
 export const BaseRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/register" element={<RegisterPage />}></Route>
-      <Route path="/login" element={<LoginPage />}></Route> */}
+      <Route path="/register" element={<RegisterPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
 
       {/* Protected Routes */}
       <Route path='/' element={<ProtectedRoute />}>
-            {/* <Route path="/dashboard" element={<DashBoardPage />}></Route> */}
+            <Route path="/dashboard" element={<DashboardPage />}></Route>
       </Route>
     </Routes>
   );
