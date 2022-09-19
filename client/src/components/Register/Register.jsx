768c57582df3737fd5ae87730e47992d2d2757bb
Isaac Olanrewaju <isaacolanre@gmail.com>
2022-09-19 22:16:24 +0100

    register done

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/icon/logo2.svg";
 import { Link } from "react-router-dom";
 import backicon from "../../assets/icon/backicon.svg";
+import { useNavigate } from "react-router-dom";
 import {
   Wrapper,
   Wrapper2,
@@ -15,7 +16,32 @@ import {
   StyledFooterButton,
 } from "../../styles/registerStyle";
 
+import { UseAuth } from "../../context/useAuth";
+
 export const Register = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    userName: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { state, register } = UseAuth();
+  console.log("outside-----" + register);
+  console.log(state.user);
+
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("here");
+    await register(formData);
+    navigate("/login");
+  };
+
   return (
     <Wrapper>
       <Wrapper2>
@@ -23,30 +49,108 @@ export const Register = () => {
 
         <HeaderAndButton>
           <Back>
-            <img
-              src={backicon}
-              style={{
-                width: 13,
-                height: 13,
-              }}
-              alt="logo"
-            />{" "}
-            Go back
+            <Link to="/">
+              <img
+                src={backicon}
+                style={{
+                  width: 13,
+                  height: 13,
+                }}
+                alt="logo"
+              />{" "}
+              Go back
+            </Link>
           </Back>
           <FormHeader>Create an account</FormHeader>
         </HeaderAndButton>
 
-        <form>
-          <StyledLabel> Name</StyledLabel>
-          <StyledInput placeholder="Enter your name"></StyledInput>
+        <form onSubmit={handleSubmit}>
+          <StyledLabel>First Name</StyledLabel>
+          <StyledInput
+            placeholder="Enter your first name"
+            type="text"
+            name="firstName"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                firstName: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
+          <StyledLabel>Last Name</StyledLabel>
+          <StyledInput
+            placeholder="Enter your last name"
+            type="text"
+            name="lastName"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                lastName: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
           <StyledLabel> Email</StyledLabel>
-          <StyledInput placeholder="Enter your email"></StyledInput>
+          <StyledInput
+            placeholder="Enter your email"
+            type="email"
+            name="email"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                email: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
+
+          <StyledLabel> Username</StyledLabel>
+          <StyledInput
+            placeholder="Enter your username"
+            type="text"
+            name="userName"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                userName: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
+
           <StyledLabel> Phone Number</StyledLabel>
-          <StyledInput placeholder="Enter your phone number"></StyledInput>
+          <StyledInput
+            placeholder="Enter your phone number"
+            type="text"
+            name="phoneNumber"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                phoneNumber: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
           <StyledLabel> Password</StyledLabel>
-          <StyledInput placeholder="Enter your password"></StyledInput>
+          <StyledInput
+            placeholder="Enter your password"
+            type="password"
+            name="password"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                password: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
           <StyledLabel> Confirm Password</StyledLabel>
-          <StyledInput placeholder="Confirm password"></StyledInput>
+          <StyledInput
+            placeholder="Confirm password"
+            type="password"
+            name="confirmPassword"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                confirmPassword: e.target.value.trim(),
+              })
+            }
+          ></StyledInput>
           <StyledFooterButton>Sign Up</StyledFooterButton>
         </form>
         <StyledFooter>
--- client/src/context/authProvider.js
+++ client/src/context/authProvider.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
@@ -10,9 +11,16 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
+    case "REGISTER":
+      return {
+        ...state,
+        user: action.data,
+      };
+
     case "LOGIN":
       return {
         ...state,
+        loggedIn: true,
         token: localStorage.getItem("token"),
         user: action.payload,
       };
@@ -23,25 +31,59 @@ function reducer(state, action) {
         token: localStorage.removeItem("token"),
       };
 
-      default:
-        throw new Error();
+    default:
+      throw new Error();
   }
 }
 
 export const AuthProvider = ({ children }) => {
-const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const login = () => {
-  dispatch({type: 'LOGIN'})
-}
+  const register = async (formData) => {
+    try {
+      const registerUser = {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        userName: formData.userName,
+        phoneNumber: formData.phoneNumber,
+        password: formData.password,
+        confirmPassword: formData.confirmPassword,
+      };
 
-const logout = () => {
-  dispatch({ type: "LOGOUT" });
-};
+      const registernewUser = async () => {
+        await axios
+          .post(`http://localhost:7000/user/register`, registerUser)
+          .then((res) => {
+            if (res.status === 201) {
+              dispatch({ type: "REGISTER", payload: res.data });
+              return;
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+      registernewUser();
+    } catch (err) {
+      throw new Error(`${err}`);
+    }
+  };
 
+  const login = (formData) => {
+    dispatch({ type: "LOGIN" });
+    const loginUser = {
+      email: formData.email,
+      password: formData.password,
+    };
+  };
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
   return (
-    <AuthContext.Provider value={{login, logout}}>
+    <AuthContext.Provider value={{ register, login, logout, state }}>
       {children}
     </AuthContext.Provider>
   );
